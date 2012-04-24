2227633acb6e4d307732f37abf4355f5bc7ca2d6
Naoto Takai <takai@recompile.net>
2012-04-25 08:20:21 +0900

    Remove ivy files and change to use jars in jpi server work dir

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,10 @@
 # -*- mode: ruby; coding: utf-8 -*-
 
-$: << File.expand_path('../../jar', __FILE__)
+$: << File.expand_path('../../work/war/WEB-INF/lib/', __FILE__)
 
-require 'commons-logging'
-require 'commons-httpclient'
+require 'commons-logging-1.1.1.jar'
+require 'commons-httpclient-3.1.jar'
+require 'jenkins-core-1.447.jar'
 
 require 'hipchat/publisher'
 
